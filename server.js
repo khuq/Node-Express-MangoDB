331301ccc112866bac8839ea2f2cea24b2eaf639
khuq <ugur_hakan@yahoo.fr>
2022-05-24 19:47:44 +0200

    Premier commit node

---
+++ server.js
@@ -0,0 +1,10 @@
+console.log('salut hakan');
+
+
+// const http= require('http');
+
+// const server = http.createServer((req, res)=>{
+//     res.end('Voila ma réponse of server!!');
+// });
+
+// server.listen(process.env.PORT || 3000);
